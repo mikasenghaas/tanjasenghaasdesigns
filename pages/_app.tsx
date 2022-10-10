99cd4c8931e41deff9ebdfc57a21b67321b11c55
jonas-mika <accounts@jonassenghaas.de>
2022-10-10 09:30:41 +0200

    feat(*): added next skeleton

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
-export default MyApp
+export default App
