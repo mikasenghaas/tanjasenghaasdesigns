cb35e72edb1cd52a49bccdc7563208e3a9884f2f
jonas-mika <accounts@jonassenghaas.de>
2022-10-10 21:38:44 +0200

    feat(theme): added custom font

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,13 +1,19 @@
 import { extendTheme } from '@chakra-ui/react'
 
-const colors = {
+const fonts = {
+  heading: `"Montserrat", "sans-serif"`,
+  body: `"Montserrat", "sans-serif"`
+}
+
+const color = {
   brand: {
     700: '#2a69ac',
     800: '#153e75',
     900: '#1a365d',
-  },
+
+  }
 }
 
-const theme = extendTheme({ colors })
+const theme = extendTheme({ color })
 
 export default theme;
