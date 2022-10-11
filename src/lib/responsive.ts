13676a5a40c44183e35e53d370c904ab16584dcb
jonas-mika <accounts@jonassenghaas.de>
2022-10-11 16:22:01 +0100

    fix(responsive): fixed ssr first render bug in responsive breakpoints

--- src/lib/responsive.ts
+++ src/lib/responsive.ts
@@ -1,9 +1,9 @@
 import { useBreakpointValue } from '@chakra-ui/react'
 
 export function useResponsiveFontSize() {
-  const sm = useBreakpointValue({ base: 'xs', md: 'sm', xxl: 'md' }, { ssr: false })
-  const md = useBreakpointValue({ base: 'sm', md: 'md', xxl: 'lg' }, { ssr: false })
-  const lg = useBreakpointValue({ base: 'md', md: 'lg', xxl: 'xl' }, { ssr: false })
+  const sm = useBreakpointValue({ base: 'xs', md: 'sm', xxl: 'md' }, { fallback: 'md', ssr: true })
+  const md = useBreakpointValue({ base: 'sm', md: 'md', xxl: 'lg' }, { fallback: 'md', ssr: true })
+  const lg = useBreakpointValue({ base: 'md', md: 'lg', xxl: 'xl' }, { fallback: 'md', ssr: true })
 
   return { sm, md, lg }
 }
