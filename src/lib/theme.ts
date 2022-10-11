cf691eabe3428fad85d965172b0da3f556ef175d
jonas-mika <accounts@jonassenghaas.de>
2022-10-11 15:07:53 +0100

    feat(theme): added custom color mode + breakpoints

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,5 +1,10 @@
 import { extendTheme } from '@chakra-ui/react'
 
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
 const fonts = {
   heading: `"Montserrat", "sans-serif"`,
   body: `"Montserrat", "sans-serif"`
@@ -14,6 +19,10 @@ const color = {
   }
 }
 
-const theme = extendTheme({ color })
+const breakpoints = {
+  xxl: "96em"
+}
+
+const theme = extendTheme({ config, color, breakpoints })
 
 export default theme;
