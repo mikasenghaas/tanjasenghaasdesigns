c03f18e412f2989cf789d1de0d24289508a74687
jonas-mika <accounts@jonassenghaas.de>
2022-10-11 15:59:20 +0100

    fix(page-container): xxl screen with small font sizes

--- src/components/page-container.tsx
+++ src/components/page-container.tsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 const PageContainer = ({ children, ...props }: any) => {
   return (
-    <Box mx="auto" px="30px" maxWidth={{ "base": "900px", "xxl": "1500px" }} {...props}>
+    <Box mx="auto" px="30px" maxWidth={{ "base": "900px", "xxl": "1200px" }} {...props}>
       {children}
     </Box>
   );
