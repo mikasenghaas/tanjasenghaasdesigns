bc15b083bd8bbaea83a32ccf4e339ff869c72c51
jonas-mika <accounts@jonassenghaas.de>
2022-10-12 09:56:11 +0100

    feat(hero): reduced parallax ease

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -12,7 +12,7 @@ export default function Hero({ url, alt }: HeroProps) {
   return (
     <>
       <Banner position='absolute' height='100vh' mt='-6%' zIndex={-1}>
-        <ScrollParallax strength={0.1} >
+        <ScrollParallax strength={0.5} lerpEase={.3}>
           <Image
             src={url}
             alt={alt}
