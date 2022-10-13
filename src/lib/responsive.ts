d552b1d7427c112eb06f4aa7ba7a9056e8c9c6b2
jonas-mika <accounts@jonassenghaas.de>
2022-10-13 19:40:16 +0100

    style(responsive): font size is the same for base and sm

--- src/lib/responsive.ts
+++ src/lib/responsive.ts
@@ -1,8 +1,8 @@
 import { useBreakpointValue } from '@chakra-ui/react'
 
 export function useResponsiveFontSize() {
-  const sm = useBreakpointValue({ base: 'xs', md: 'sm', xxl: 'md' }, { fallback: 'md', ssr: true })
-  const md = useBreakpointValue({ base: 'sm', md: 'md', xxl: 'lg' }, { fallback: 'md', ssr: true })
+  const sm = useBreakpointValue({ base: 'sm', md: 'sm', xxl: 'md' }, { fallback: 'md', ssr: true })
+  const md = useBreakpointValue({ base: 'md', md: 'md', xxl: 'lg' }, { fallback: 'md', ssr: true })
   const lg = useBreakpointValue({ base: 'md', md: 'lg', xxl: 'xl' }, { fallback: 'md', ssr: true })
   const xl = useBreakpointValue({ base: 'lg', md: 'xl', xxl: '2xl' }, { fallback: 'md', ssr: true })
 
