a80ac2a90facd4dd6b1ab48d0701c78337822766
jonas-mika <accounts@jonassenghaas.de>
2022-10-14 16:24:18 +0100

    feat(index): added responsive hero text + animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,13 +12,14 @@ import typographyById, { Typography } from '@/models/typography'
 import corporateById, { Corporate } from '@/models/corporate'
 
 import { useResponsiveFontSize } from '@/lib/responsive'
+import { MotionHeading } from '@/components/motion'
 
 const Home: NextPage = () => {
   const magazines = Object.values(magazinesById)
   const typography = Object.values(typographyById)
   const corporate = Object.values(corporateById)
 
-  const { lg, xl } = useResponsiveFontSize()
+  const { xl } = useResponsiveFontSize()
 
   return (
     <>
@@ -30,26 +31,44 @@ const Home: NextPage = () => {
       </Head>
 
       <Layout heroUrl='/assets/hero/index.jpg'>
-        <Box position='absolute' mt='-400px' ml={-40}>
-          <Heading mb={3}>Gutes Design ist nicht nur schön.</Heading>
-          <Heading mb={3}> Es gibt Inhalten eine Form. </Heading>
-          <Heading>Und zeigt damit Haltung.</Heading>
+        <Box
+          position='absolute'
+          mt={{ base: '-300px', sm: '-350px', md: '-350px', xl: '-500px' }}
+          ml={{ base: 0, xl: -40 }}
+        >
+          <MotionHeading
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0, transition: { delay: .5, duration: .5 } }}
+            color='gray.800'
+            fontSize={{ base: '4xl', sm: '5xl', md: '6xl', lg: '6xl', xl: '6xl', xxl: '7xl' }}
+          >
+            Gutes Design ist <br />
+            mehr als nur schön.<br />
+          </MotionHeading>
+          <MotionHeading
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0, transition: { delay: 1.5, duration: .5 } }}
+            color='gray.800'
+            fontSize={{ base: '4xl', sm: '5xl', md: '6xl', lg: '6xl', xl: '6xl', xxl: '7xl' }}
+          >
+            Es ist eine Haltung.
+          </MotionHeading>
         </Box>
         <Box >
-          <Heading id='magazines' fontSize={xl} pt={20} mb={10} >Magazinentwicklung</Heading>
-          <Grid templateColumns={{ base: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)', md: 'repeat(3, 1fr)' }} gap={5}>
+          <Heading id='magazines' fontSize={xl} fontWeight={800} pt={20} mb={10} >Magazinentwicklung</Heading>
+          <Grid templateColumns={{ base: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)', md: 'repeat(3, 1fr)' }} gap='40px 20px'>
             {
               magazines.map((magazine: Magazine) => <GridImage key={magazine.id} project={magazine} type='magazine' />)
             }
           </Grid>
-          <Heading id='corporate' fontSize={xl} pt={20} mb={10} >Corporate Design</Heading>
-          <Grid templateColumns={{ base: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)', md: 'repeat(3, 1fr)' }} gap={5}>
+          <Heading id='corporate' fontSize={xl} fontWeight={800} pt={20} mb={10} >Corporate Design</Heading>
+          <Grid templateColumns={{ base: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)', md: 'repeat(3, 1fr)' }} gap='40px 20px'>
             {
               corporate.map((corporate: Corporate) => <GridImage key={corporate.id} project={corporate} type='magazine' />)
             }
           </Grid>
-          <Heading id='typography' fontSize={xl} pt={20} mb={10}>Typografie</Heading>
-          <Grid templateColumns={{ base: 'repeat(1, 1fr)', sm: 'repeat(3, 1fr)', md: 'repeat(4, 1fr)' }} gap={5}>
+          <Heading id='typography' fontSize={xl} fontWeight={800} pt={20} mb={10}>Typografie</Heading>
+          <Grid templateColumns={{ base: 'repeat(1, 1fr)', sm: 'repeat(3, 1fr)', md: 'repeat(4, 1fr)' }} gap='40px 20px'>
             {
               typography.map((typographyProject: Typography) => <GridImage key={typographyProject.id} project={typographyProject} type='typography' />)
             }
