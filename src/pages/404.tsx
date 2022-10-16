721956797b7f20232f6e98e6bdd85134092e0fb3
jonas-mika <accounts@jonassenghaas.de>
2022-10-16 22:11:38 +0100

    fix(404): exchange png to svg
    fixed issue that image background on 404 page was white instead of transparent
    
    #4

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -18,28 +18,26 @@ const NotFound: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Layout>
-        <Flex mt={40} height="50vh" direction="column" alignItems="center">
-          <Box position="relative" flex={1} w="100%">
-            <Image
-              src="/assets/not-found.png"
-              alt="not-found"
-              layout="fill"
-              objectFit="scale-down"
-            />
-          </Box>
-          <Heading fontSize={xl} textAlign="center" my={5}>
-            Sorry... something went wrong
-          </Heading>
-          <Box fontSize={md} textAlign="center">
-            This URL does not seem to exist. Go back home{' '}
-            <Link href="/" passHref>
-              <ChakraLink>here</ChakraLink>
-            </Link>
-            .
-          </Box>
-        </Flex>
-      </Layout>
+      <Flex mt={40} height="50vh" direction="column" alignItems="center">
+        <Box position="relative" flex={1} w="100%">
+          <Image
+            src="/assets/not-found.svg"
+            alt="not-found"
+            layout="fill"
+            objectFit="scale-down"
+          />
+        </Box>
+        <Heading fontSize={xl} textAlign="center" my={5}>
+          Sorry... something went wrong
+        </Heading>
+        <Box fontSize={md} textAlign="center">
+          This URL does not seem to exist. Go back home{' '}
+          <Link href="/" passHref>
+            <ChakraLink>here</ChakraLink>
+          </Link>
+          .
+        </Box>
+      </Flex>
     </>
   )
 }
