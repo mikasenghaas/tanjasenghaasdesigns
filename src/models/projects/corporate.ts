aacb8e4c74a48a11b63289ca8c77c555fed8e56f
jonas-mika <accounts@jonassenghaas.de>
2022-10-27 11:03:50 +0200

    feat(models): added two corporate projects

--- src/models/projects/corporate.ts
+++ src/models/projects/corporate.ts
@@ -11,6 +11,24 @@ const DATA: ProjectsById = {
       'Wer sind die Entscheiderinnen und Entscheider des Jahres 2030? Um diese Frage zu beantworten, hat die Hager Unternehmensberatung mit einem der namhaftesten Zukunfts-Institute Deutschlands zusammengearbeitet: dem Thinktank 2bAHEAD mit Sitz in Leipzig. Sie beantragte die Agentur Narrative Impact mit dem Herausarbeiten des Inhalts. Daraus entstand ein interessantes und optisch anspruchsvolles Whitepaper.'
     ],
     numImages: 4
+  },
+  'landlust-shop-beihefter': {
+    id: 'landlust-shop-beihefter',
+    name: 'Landlust Shop Beihefter',
+    date: new Date('2022'),
+    position: 'Art Direktion',
+    company: 'Deutsche Medien-Manufaktur',
+    description: ["Um in der Weihnachtszeit auf die Angebote des Landlust Shops aufmerksam zu machen, entstand ein 8-seitiger Beihefter, der in Landlust 6/22 eingeheftet war."],
+    numImages: 4
+  },
+  'landlust-shop-anzeige': {
+    id: 'landlust-shop-anzeige',
+    name: 'Landlust Shop Anzeige',
+    date: new Date('2022'),
+    position: 'Art Direktion',
+    company: 'Deutsche Medien-Manufaktur',
+    description: ["Überarbeitung der Eigenanzeige des Landlust-Shops inklusive neuem Landlust-Shop Logo."],
+    numImages: 2
   }
 }
 
